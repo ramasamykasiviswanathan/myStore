c10fcbaad11bb2645443ea89db538eff5cc092fd
Ramasamy Kasiviswanathan <kramasamy1991@gmail.com>
2018-03-09 01:56:04 +0530

    Changes for api xml marshalling

--- api/src/main/java/com/doordelivr/ApplicationStartup.java
+++ api/src/main/java/com/doordelivr/ApplicationStartup.java
@@ -1,6 +1,7 @@
 package com.doordelivr;
 
 import java.math.BigDecimal;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -12,8 +13,13 @@ import org.springframework.stereotype.Component;
 
 import com.doordelivr.mssql.dao.crudrepository.ProductRepository;
 import com.doordelivr.mssql.dao.model.Product;
+import com.doordelivr.mssql.dao.xml.model.InputParam;
+import com.doordelivr.mssql.dao.xml.model.OrderDetails;
+import com.doordelivr.mssql.dao.xml.model.SaveOrder;
 import com.doordelivr.mysql.dao.crudrepository.SymbolEntityRepository;
 import com.doordelivr.mysql.dao.model.SymbolEntity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent>{
@@ -23,9 +29,19 @@ public class ApplicationStartup implements ApplicationListener<ApplicationReadyE
 	
 	@Autowired
 	private SymbolEntityRepository symbols;
+	
+	@Autowired
+	private ObjectMapper xmlobjectMapper;
 
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent event) {
+		try {
+			String string = xmlobjectMapper.writeValueAsString(populateObject());
+			System.out.println(string);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		List<? super Object> list = products.Products(0);
 		List<Product> products = (List<Product>) list.parallelStream().filter(Objects::nonNull).map( v ->{
 			Object[] value = (Object[])v;
@@ -41,4 +57,34 @@ public class ApplicationStartup implements ApplicationListener<ApplicationReadyE
 		symbolEntity.setSymbol("symbol");
 		symbols.save(symbolEntity);
 	}
+	
+	InputParam populateObject(){
+		InputParam inputParam = new InputParam();
+		inputParam.setAppID("1");
+		inputParam.setSpType("SaveOrder");
+		inputParam.setUserID("");
+		inputParam.setRetailerID("");
+		inputParam.setOrder(new SaveOrder());
+		inputParam.getOrder().setDetails(new LinkedList<>());
+		
+		OrderDetails orderDetails = new OrderDetails();
+		orderDetails.setImageLink("");
+		orderDetails.setOrderNO("20");
+		orderDetails.setOrderDate("2017-12-28");
+		orderDetails.setProductID("100001");
+		orderDetails.setPrice("10");
+		orderDetails.setQuantity("10");
+		inputParam.getOrder().getDetails().add(orderDetails);
+		orderDetails = new OrderDetails();
+		orderDetails.setImageLink("");
+		orderDetails.setOrderNO("20");
+		orderDetails.setOrderDate("2017-12-28");
+		orderDetails.setProductID("100002");
+		orderDetails.setPrice("50");
+		orderDetails.setQuantity("5");
+		inputParam.getOrder().getDetails().add(orderDetails);
+		
+		return inputParam;
+	}
+	
 }
