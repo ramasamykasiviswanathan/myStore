bbfa9ffff867e48c6f6d0e847e6170b23989a8a4
Ramasamy Kasiviswanathan <kramasamy1991@gmail.com>
2018-03-12 21:45:34 +0530

    Quantity & Category Product

---
+++ web/frontend/src/app/feature/category-product/category-product.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-product',
+  templateUrl: './category-product.component.html',
+  styleUrls: ['./category-product.component.scss']
+})
+export class CategoryProductComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
