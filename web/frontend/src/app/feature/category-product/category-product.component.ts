25bcc01eea362ff2eb3841b0b60a9f6412f402bb
Ramasamy Kasiviswanathan <kramasamy1991@gmail.com>
2018-03-12 22:31:36 +0530

    Updated Category component.

--- web/frontend/src/app/feature/category-product/category-product.component.ts
+++ web/frontend/src/app/feature/category-product/category-product.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-product.component.scss']
 })
 export class CategoryProductComponent implements OnInit {
+  values: String[];
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.values = [
+      '2% Fat Milk',
+      'Low Fat Milk',
+      'Fat-Free Milk',
+      'Whole Lactose Reduced Milk',
+      '2% Fat Lactose Reduced Milk',
+      'Fat-Free Lactose Reduced Milk',
+      '2% Fat Lactose-Free Milk',
+      'Fat-Free Lactose-FreeMilk',
+      'Powdered (Dry) Milk',
+      'Nonfat Powdered (Dry) Milk',
+      'Evaporated Milk'
+    ];
+  }
 }
